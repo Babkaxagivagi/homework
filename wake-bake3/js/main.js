768c1b3095984845127e1d4d6d5295099b9d11e1
khuma <99593030+Babkaxagivagi@users.noreply.github.com>
2025-11-02 20:49:51 +0900

    Add files via upload

---
+++ wake-bake3/js/main.js
@@ -0,0 +1,110 @@
+// const body = document.querySelector('.body')
+// const menuBtn = document.querySelector('.burger-icon')
+
+// menuBtn.addEventListener('click', (e) =>{
+//   body.classList.toggle('body--opened-menu')
+// })
+
+(function(){
+  document.addEventListener('click', burgerInit)
+  function burgerInit(e){
+    const burgerIcon = e.target.closest('.burger-icon')
+    const burgerNavLink = e.target.closest('.nav__link')
+    console.log('burgerIcon')
+    if(!burgerIcon && !burgerNavLink) return
+    if(document.documentElement.clientWidth > 900) return
+    if(!document.body.classList.contains('body--opened-menu')){
+      document.body.classList.add('body--opened-menu')
+    } else{
+      document.body.classList.remove('body--opened-menu')
+    }
+  }
+  const btnOpen = document.querySelector('.about__img-button')
+
+
+
+  const body = document.querySelector('.body')
+
+  const openModal = (e) =>{
+    e.preventDefault()
+    body.classList.add('body--opened-modal')
+  }
+  const closeModal = (e) =>{
+    body.classList.remove('body--opened-modal')
+  }
+  btnOpen.addEventListener('click', openModal)
+  body.addEventListener('click', event =>{
+    const target = event.target
+    event.preventDefault()
+    if (target && target.closest('.modal__cancel') || target.classList.contains('modal')){
+      closeModal()
+    }
+  })
+
+  document.addEventListener('keydown', event =>{
+    if(event.code === 'Escape' && body.classList.contains('body--opened-modal')){
+      closeModal()
+    }
+  })
+
+
+
+  const tabControls = document.querySelector('.tab-controls')
+  tabControls.addEventListener('click', toggleTab)
+
+  function toggleTab(e){
+    const tabControl = e.target.closest('.tab-controls__link') // проверка есть ли у target родитель с классом .tab-controls__link и записывает в переменную если есть 
+
+    if(!tabControl) return
+    e.preventDefault() // прописывать если никуда перекидывать не надо, на другие страницы
+    if(tabControl.classList.contains('tab-controls__link--active')) return
+
+    const tabContentID = tabControl.getAttribute('href')
+    const tabContent = document.querySelector(tabContentID)
+    const activeControl = document.querySelector('.tab-controls__link--active')
+    const activeContent = document.querySelector('.tab-content--show')
+
+    if(activeControl){
+      activeControl.classList.remove('tab-controls__link--active')
+    }
+    if(activeContent){
+      activeContent.classList.remove('tab-content--show')
+    }
+    
+    tabContent.classList.add('tab-content--show')
+    tabControl.classList.add('tab-controls__link--active')
+
+  }
+
+
+
+
+  // Accordion
+  const accordionLists = document.querySelectorAll('.accordion-list');
+  accordionLists.forEach(el =>{
+    el.addEventListener('click', (e) =>{
+      const accordionList = e.currentTarget
+      const accordionOpenedItem = accordionList.querySelector('.accordion-list__item--opened')
+      const accordionOpenedContent = accordionList.querySelector('.accordion-list__item--opened .accordion-list__control')
+      console.log(accordionOpenedItem)
+      const accordionControl = e.target.closest('.accordion-list__control');
+      if(!accordionControl) return
+      const accordionItem = accordionControl.parentElement;
+      const accordionContent = accordionControl.nextElementSibling;
+      if(accordionOpenedItem && accordionOpenedItem != accordionItem){
+        accordionOpenedItem.classList.remove('accordion-list__item--opened');
+        accordionOpenedContent.style.maxHeight = null;
+
+      }
+      accordionItem.classList.toggle('accordion-list__item--opened');
+      if(accordionItem.classList.contains('accordion-list__item--opened')){
+        accordionContent.style.maxHeight = accordionContent.scrollHeight + 'px';
+      } else{
+        accordionContent.style.maxHeight = null;
+      }
+    })
+  })
+})()
+
+
+
